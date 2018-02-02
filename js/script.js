3fbc03cd6ba50af0fd64129344465869ecac4f67
Bartłomiej Bajda <allerbbb@gmail.com>
2018-02-02 15:31:04 +0000

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var a = prompt("Podaj długość podstawy trójkąta");
+var h = prompt("Podaj długość wysokości trójkąta");
+var triangleArea = a*h/2;
+alert('Pole takiego trójkąta to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
